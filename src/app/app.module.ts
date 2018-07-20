07d1e6aaccfce7311759e738e8c4c3804deaaf23
simon <simone.moraes77@gmail.com>
2018-07-20 16:18:01 -0300

    Primeiro Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
+
 
 import { AppComponent } from './app.component';
+import { SocialComponent } from './layout/social/social.component';
+import { MenuComponent } from './layout/menu/menu.component';
+import { HomeComponent } from './home/home.component';
+import { ProdutosComponent } from './produtos/produtos.component';
+import { ServicosComponent } from './servicos/servicos.component';
+import { PacotesServicosComponent } from './pacotes-servicos/pacotes-servicos.component';
+import { PortifolioComponent } from './portifolio/portifolio.component';
+import { ContatoComponent } from './contato/contato.component';
+import { RodapeComponent } from './layout/rodape/rodape.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SocialComponent,
+    MenuComponent,
+    HomeComponent,
+    ProdutosComponent,
+    ServicosComponent,
+    PacotesServicosComponent,
+    PortifolioComponent,
+    ContatoComponent,
+    RodapeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+      AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/pacotes-servicos/pacotes-servicos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pacotes-servicos',
+  templateUrl: './pacotes-servicos.component.html',
+  styleUrls: ['./pacotes-servicos.component.css']
+})
+export class PacotesServicosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
